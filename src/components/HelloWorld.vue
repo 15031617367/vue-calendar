a45dc64b1d86bdae15f3e673bb5ac94f1ed2bd7d
李承斌 <1307437149@qq.com>
2018-11-20 17:59:29 +0800

    index

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+
+     <vue-event-calendar :events="demoEvents"></vue-event-calendar>
+   
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+         demoEvents: [
+         {
+          date: '2018/7/2',
+          title: '考试成绩',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, omnis accusamus quidem ullam ea voluptatem itaque qui ad dignissimos optio, nostrum tempore dolores odio nulla reiciendis consequuntur ipsum quis nobis?'
+        },
+        {
+          date: '2018/7/4',
+          title: '全校通报'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
